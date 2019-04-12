896cb42dab8c5998ae9c30438c2a4092df897553
Unknown <audreygan@utexas.edu>
2019-04-12 13:50:54 -0500

    more code on part A

--- lab6A/lab6A.srcs/sources_1/new/fp_multiplier.v
+++ lab6A/lab6A.srcs/sources_1/new/fp_multiplier.v
@@ -6,28 +6,24 @@ input [3:0] F1;
 input [3:0] E1;
 input [3:0] F2;
 input [3:0] E2;
-output [6:0] F;
-output V;
-output done;
-
-reg [6:0] F;
-reg done;
-reg V;
+output reg [6:0] F;
+output reg V;   //overflow indicator
+output reg done;    //floating point multiplication compelte
 
 reg [3:0] A;
 reg [3:0] B;
 reg [3:0] C;
 reg [4:0] X;
 reg [4:0] Y;
-reg Load;
-reg Adx;
-reg SM8;
-reg RSF;
-reg LSF;
+reg Load;   //load F1, E1, F2, E2 to appropriate registers
+reg Adx;    //add exponents, starts fraction multiplier
+reg SM8;    //set exponent to -2 (handle special case of 0)
+reg RSF;    //shift fraction right, increment E
+reg LSF;    //shift fraction left, decrement E
 reg AdSh;
 reg Sh;
 reg Cm;
-reg Mdone;
+reg Mdone;  //fraction multiply done
 reg [1:0] PS1;
 reg [1:0] NS1;
 reg [2:0] State;
@@ -40,6 +36,7 @@ initial begin
     Nextstate = 0;
 end
 
+//main always statement, generate control signals based on SM chart
 always @(PS1 or St or Mdone or X or A or B) 
 begin: main_control
     Load = 1'b0;
@@ -54,7 +51,7 @@ begin: main_control
     case(PS1)
         0:
             begin
-                F = 7'b0000000;
+                F = 7'b0000000; //initialize
                 done = 1'b0;
                 V = 1'b0;
                 if(St == 1'b1)
@@ -65,35 +62,35 @@ begin: main_control
             end
         1:
             begin
-                Adx = 1'b1;
+                Adx = 1'b1; //set flag to add
                 NS1 = 2;
             end
         2:
             begin
-                if(Mdone == 1'b1)
+                if(Mdone == 1'b1)   //check if fractino multiply done
                     begin
-                        if(A == 0)
+                        if(A == 0)  //special case of 0
                         begin
-                            SM8 = 1'b1;
+                            SM8 = 1'b1; //set exponent to -8 for special case of 0
                         end
                         else if (A == 4 & B == 0)
                         begin
-                            RSF = 1'b1;
+                            RSF = 1'b1; //shift fraction right, increment exponent
                         end
                         else if (A[2] == A[1])
                         begin
-                            LSF = 1'b1;
+                            LSF = 1'b1; //shift fraction left, decrement exponent
                         end
-                        NS1 = 3;
+                        NS1 = 3;    //go to next state
                     end
                 else
                     begin
-                        NS1 = 2;
+                        NS1 = 2;    //keep staying at this state until fraction multiply done
                     end
             end
         3:
             begin
-                if(X[4] != X[3])
+                if(X[4] != X[3])    //check for overflow
                     begin
                         V = 1'b1;
                     end
@@ -101,9 +98,9 @@ begin: main_control
                     begin
                         V = 1'b0;
                     end
-                done = 1'b1;
-                F = {A[2:0], B};
-                if(St == 1'b0)
+                done = 1'b1;    //finished multiplication
+                F = {A[2:0], B};    //set fraction
+                if(St == 1'b0)  //reset statemachine if start signal low
                     begin
                         NS1 = 0;
                     end
@@ -111,17 +108,18 @@ begin: main_control
     endcase
 end
 
+//generates control signals for fraction multiplier
 always @(State or Adx or B)
 begin: mul2c
     AdSh = 1'b0;
     Sh = 1'b0;
     Cm = 1'b0;
     Mdone = 1'b0;
-    Nextstate = 0;
+    Nextstate = 0;  //initialize
     case(State)
         0:
             begin
-                if(Adx == 1'b1)
+                if(Adx == 1'b1) //finished adding exponent, start multiply fraction
                 begin
                     if((B[0]) == 1'b1)
                         begin
@@ -161,42 +159,43 @@ begin: mul2c
             end
         4: 
             begin
-                Mdone = 1'b1;
-                Nextstate = 0;
+                Mdone = 1'b1;   //finish multiplying
+                Nextstate = 0;  //restart state machine
             end
     endcase
 end
 
-wire [3:0] addout;
+wire [3:0] addout;  //represents output of 4 bit full adder for fraction multiplier
 assign addout = (Cm == 1'b0)? (A+C) : (A-C);
 
+//tests the control signals and updates the appropriate registers on rising edge of clock
 always @(posedge clk)
 begin : update
     PS1 <= NS1;
     State <= Nextstate;
     if(Load == 1'b1)
         begin
-            X <= {E1[3], E1};
-            Y <= {E2[3], E2};
+            X <= {E1[3], E1}; //sign extend exponent 1
+            Y <= {E2[3], E2}; //sign extend exponent 2
             A <= 4'b0000;
-            B <= F1;
-            C <= F2;
+            B <= F1;    //B = fraction 1
+            C <= F2;    //C = fraction 2
         end
     if(Adx == 1'b1)
         begin
-            X <= X+Y;
+            X <= X+Y;   //add exponents
         end
     if(SM8 == 1'b1)
         begin
             X <= 5'b11000;
         end
-    if(RSF == 1'b1)
+    if(RSF == 1'b1) //shift fraction right, increment exponent
         begin
             A <= {1'b0, A[3:1]};
             B <= {A[0], B[3:1]};
             X <= X+1;
         end
-    if(LSF == 1'b1)
+    if(LSF == 1'b1) //shift fraction left, decrement exponent
         begin
             A <= {A[2:0], B[3]};
             B <= {B[2:0], 1'b0};
--- lab6A/lab6A.srcs/sources_1/new/mac_operator.v
+++ lab6A/lab6A.srcs/sources_1/new/mac_operator.v
@@ -1,6 +1,29 @@
-module mac_operator(value, inputA, inputB);
+module mac_operator(clk, value, inputA, inputB);
+input clk;
 output[7:0] value;
 input[7:0] inputA;
 input[7:0] inputB;
 
+//wire Asign = inputA[7];
+wire [3:0] Aexp = inputA[7:4];  //4 bit exponent
+wire [4:0] Afrac = inputA[3:0]; //4 bit fraction
+
+//wire Bsign = inputB[7];
+wire [3:0] Bexp = inputB[7:4];  //4 bit exponent
+wire [4:0] Bfrac = inputB[3:0]; //4 bit fraction
+
+reg St; //start flag output for mult
+reg [6:0] F;    //output
+reg done1;  //done flag
+reg V;  //overflow flag
+
+reg St2;    //start input for add
+reg done2;  //done flag 2
+reg ovf;    //overflow flag
+reg unf;    //underflow flag
+reg FPinput;    //floating point input
+reg FPsum;  //floating point sum/output
+
+multiply fp_multiplier(clk, St, Afrac, Aexp, Bfrac, Bexp, F, V, done1);
+add fp_adder(clk, St, done2, ovf, unf, FPinput, FPsum);
 endmodule
